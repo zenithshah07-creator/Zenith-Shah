42eaf713b0dcdb93f24473d0244e589d478ca831
Zenith Shah <zenithshah07@gmail.com>
2026-03-09 18:40:16 +0545

    fix: remove hashtag from projects URL in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,12 +19,12 @@ const Navbar = () => {
     }, []);
 
     const navLinks = [
-        { name: 'Home', href: '/#home' },
-        { name: 'About', href: '/#about' },
-        { name: 'Skills', href: '/#Skills' },
-        { name: 'Projects', href: '/#projects' },
-        { name: 'Experience', href: '/#Experience' },
-        { name: 'Contact', href: '/#contact' },
+        { name: 'Home', href: '/home' },
+        { name: 'About', href: '/about' },
+        { name: 'Skills', href: '/skills' },
+        { name: 'Projects', href: '/projects' },
+        { name: 'Experience', href: '/experience' },
+        { name: 'Contact', href: '/contact' },
     ];
 
     return (
