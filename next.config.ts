5c395f0b7d09471683808b0d90d782a97f89b0c6
Zenith Shah <zenithshah07@gmail.com>
2026-03-10 16:37:39 +0545

    chore: update next.config.ts for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
-  output: 'export',
-  basePath: isProd ? '/Zenith-Shah' : '',
-  assetPrefix: isProd ? '/Zenith-Shah' : '',
   images: {
     unoptimized: true,
   },
